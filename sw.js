390f8177c73c17803eaca9d4286101f2e3963a43
gomis89-droid <gomis89@gmail.com>
2026-04-19 03:38:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'repaso-safe-v2';
+const CACHE_VERSION = 'registro-mobile-tabs-v3';
 const CORE_ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event => {
@@ -48,3 +48,5 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
+// cache bump mobile tabs v4
